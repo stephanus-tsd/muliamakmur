9ec22dff566d5054b11588cadfff2dc991daff25
samueljulius <sammy.strats@gmail.com>
2012-07-30 20:33:58 +0700

    layout tambah and view

--- Layout/index.php
+++ Layout/index.php
@@ -18,7 +18,7 @@
 
 <div id="content-main">
 
-      		<div id="content-form" align="left">
+<div id="content-form" align="left">
 				<form name="">
 				<table style="border:1px solid #000000; margin-top:5px; margin-left:5px">
 				<tr>
@@ -36,14 +36,25 @@
                 </tr>
                 </table>
                 </form>
-			</div>
+	  </div>
     </div>
     
     <div id="content-table">
     </div>
     
     <div id="content-menu">
-     <fieldset>
+    
+    
+     <div id="TabbedPanels1" class="TabbedPanels">
+       <ul class="TabbedPanelsTabGroup">
+         <li class="TabbedPanelsTab" tabindex="0">Tab 1</li>
+         <li class="TabbedPanelsTab" tabindex="0">Tab 2</li>
+       </ul>
+       <div class="TabbedPanelsContentGroup">
+         <div class="TabbedPanelsContent">Content 1
+         
+         
+         <fieldset>
      <legend>[ Tambah data ]</legend>
      <table>
      <form >
@@ -52,6 +63,66 @@
      </tr>
      <tr>
      <td>Tanggal PO </td>
+     <td>
+     <select>
+     <?php
+		for($i=1; $i<32; $i++)
+		print ("<option value=$i>$i</option>");
+	 ?>
+     </select>
+     </td>
+     <td>Bulan</td>
+     <td>
+     <?php
+		$month = array(
+		"01"=>"Januari",
+		"02"=>"Februari",
+		"03"=>"Maret",
+		"04" =>"April",
+		"05"=>"Mei",
+		"06"=>"Juni",
+		"07"=>"Juli",
+		"08"=>"Agustus",
+		"09"=>"September",
+		"10"=>"Oktober",
+		"11"=>"November",
+		"12"=>"Desember");
+		?>
+        <?php
+		echo "<select>";
+		foreach ($month as $key=>$value){
+		echo '<option value="'.$key.'"';
+		if($month==$key) echo 'selected';
+		echo '>'.$value.'</option>';
+		}
+		echo '</select>';
+		?>
+     </td>
+     <td>Tahun</td>
+     <td>
+     <?
+		echo "<select name='tanggal'>";
+		for ($i=2012;$i<=20;$i++) {
+		echo "<option value=$i selected>$i</option> ";
+		}
+		echo "</select>";
+	?>
+     </td>
+     </tr>
+     <tr>
+     <td>Pemasok</td><td><input type="text"/></td>
+     </tr>
+     <tr>
+     <td>Alamat</td><td><input type="text" height="70" width="200" /></td>
+     </tr>
+     <tr>
+     <td>Up</td><td><input type="text"/></td>
+     </tr>
+     <tr>
+     <td>Tipe Pemasok<td><select><option>PPN</option><option>Bukan PPN</option></select></td>
+     </tr>
+     <tr>
+     <td>Dateline Pengiriman</td>
      <td>
      <?php
 	 	print ("<select>");
@@ -91,34 +162,11 @@
      <td>
      <?
 		echo "<select name='tanggal'>";
-		for ($i=2012;$i<=2012;$i++) {
+		for ($i=2012;$i<=20;$i++) {
 		echo "<option value=$i selected>$i</option> ";
 		}
 		echo "</select>";
 	?>
-     </td>
-     </tr>
-     <tr>
-     <td>Pemasok</td><td><input type="text"/></td>
-     </tr>
-     <tr>
-     <td>Alamat</td><td><input type="text" height="70" width="200" /></td>
-     </tr>
-     <tr>
-     <td>Up</td><td><input type="text"/></td>
-     </tr>
-     <tr>
-     <td>Tipe Pemasok<td><select><option>PPN</option><option>Bukan PPN</option></select></td>
-     </tr>
-     <tr>
-     <td>Dateline Pengiriman</td>
-     <td>
-     <?php
-	 	print ("<select>");
-		for($i=1; $i<32; $i++)
-		print ("<option value=$i>$i</option>");
-		print ("</select>");
-		?>
      </td>
      </tr>
      <tr>
@@ -131,11 +179,23 @@
      </form>
      </table>
      </fieldset>
+
+         
+         </div>
+         <div class="TabbedPanelsContent">Content 2</div>
+       </div>
      </div>
+    
+          </div>
   </div>
 <div id="footer">
 <div id="footer-main"><b><i>Copyright 2012 @ Creative Spirit. All Right Reserved</i></b></div>
 </div>
 </div>
+<script type="text/javascript">
+<!--
+var TabbedPanels1 = new Spry.Widget.TabbedPanels("TabbedPanels1");
+//-->
+</script>
 </body>
 </html>
